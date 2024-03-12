60abf4aad312d0afd28b2e73ecbebc15de27f475
Yordinaa <105152673+Yordinaa@users.noreply.github.com>
2024-03-12 19:46:10 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -16,10 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# Importing required libraries
+from . import views
 
+
+# Url patterns for Books app module of Library Management System
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
+     path("", views.home, name="home"),
+    path("issue", views.issue, name="issue"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("logout", views.logout, name="logout"),
+    path("return_item", views.return_item, name="return_item"),
+    path("history", views.history, name="history"),
 ]
 
 
